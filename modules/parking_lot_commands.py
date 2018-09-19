e889bab1fdf58ae4de14fdbbdf7f622b05a5e15a
Divyanshu Rawat <divyanshu.r46956@gmail.com>
2018-09-19 06:06:13 +0530

    written logic for handling file inputs and user inputs

--- modules/parking_lot_commands.py
+++ modules/parking_lot_commands.py
@@ -9,12 +9,34 @@ class Parking_commands():
 		self.parking_lot_functions_class = parking_lot_functions_class.Parking_lot()
 
 	def input_file_processing(self,entered_file):
-		print 'I am in input_file_processing'
+		read_file_obj = open(entered_file)  					#Open a file, return an object of the file.
+		try:
+			while True:
+				current_line = read_file_obj.next()				#This method returns the next input line, or raises StopIteration when EOF is hit.
+				if current_line.endswith('\n'):
+					current_line = current_line[:-1]			#It slices the string to omit the last character, in this case a newline character.
+				if current_line == '':
+					continue
+				self.process_entered_commands(current_line)
+		except IOError:
+			print 'File not found'
+		except StopIteration:
+			read_file_obj.close()
+		except Exception as error:
+			print "Error occured while processing file %s" % error.message
 
 	def entered_input(self):
-		print 'I am in entered_input'
+		try:
+			while True:
+				standard_input = raw_input("Enter Command:")
+				self.process_entered_commands(standard_input)
+		except(KeyboardInterrupt, SystemExit): 					#Raised when the user hits the interrupt key (normally Control-C or Delete)
+			return
+		except Exception as error:
+			print "Error occured while processing file %s" % error.message
 
-	def process_entered_commands(self):
+
+	def process_entered_commands(self,standard_input):
 		print 'I am in process_entered_commands'
 
 # defining this file as the entry point to our program,
